67a0ad5c8f5339b1b6e52cc5b5eef9a06f45b0da
Yutaka Nishimura <ytk.nishimura@gmail.com>
2018-08-19 13:32:24 +0900

    update 8.11.4-4.1.2

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-docker build -t ynishi/firebase-docker:8.11.3-4.0.3-3 . --no-cache
+NODE_VERSION=8.11.4
+FIREBASE_TOOLS_VERSION=4.1.2
+IMAGE_VERSION=1
+TAG_VERSION=${NODE_VERSION}-${FIREBASE_TOOLS_VERSION}-${IMAGE_VERSION}
+
+cat Dockerfile.template | \
+  sed "s/@NODE_VERSION@/${NODE_VERSION}/g" | \
+  sed "s/@FIREBASE_TOOLS_VERSION@/${FIREBASE_TOOLS_VERSION}/g" | \
+  sed "s/@TAG_VERSION@/${TAG_VERSION}/g" > \
+  Dockerfile
+
+docker build -t ynishi/firebase-docker:${TAG_VERSION} . --no-cache
 docker build -t ynishi/firebase-docker:latest .
